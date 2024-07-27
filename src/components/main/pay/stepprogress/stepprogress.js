62d8d7204f3dd3b6415b632ddac0cb17e46e9560
Lan956616 <cd25924218@gmail.com>
2024-07-27 11:09:20 +0800

    complete content of pay-stepprogress

--- src/components/main/pay/stepprogress/stepprogress.js
+++ src/components/main/pay/stepprogress/stepprogress.js
@@ -1,7 +1,15 @@
+import styles from './stepprogress.module.css'
+
 export default function StepProgress () {
   return(
-    <div>
-      StepProgressStepProgress
+    <div className={styles.StepProgress}>
+      <p className={styles.StepProgressTitle}>結帳</p>
+      <ul className={styles.progress}>
+        <li className={`${styles.progressItem} ${styles.Item1}`}>寄送地址</li>
+        <li className={`${styles.progressItem} ${styles.Item2}`}>運送方式</li>
+        <li className={`${styles.progressItem} ${styles.Item3}`}>付款資訊</li>
+      </ul>
+    
     </div>
   )
 }
