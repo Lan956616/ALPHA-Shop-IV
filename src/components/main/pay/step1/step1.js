dfa7bb58ae2e38fc2280dbcd62abb3dec7e7d34a
Lan956616 <cd25924218@gmail.com>
2024-07-30 22:56:50 +0800

    complete content of step 1 (without select rectangle)

--- src/components/main/pay/step1/step1.js
+++ src/components/main/pay/step1/step1.js
@@ -1,5 +1,41 @@
+import styles from './step1.module.css'
+
+
+function Formrow({children}) {
+  return(
+    <div className={styles.Formrow}>
+      {children}
+    </div>
+
+  )
+}
+
+function FormInput({width, title, placeholder}) {
+  return(
+    <div className={styles.FormInput} style={{width:width}}>
+      <p className={styles.FormInputTitle}>{title}</p>
+      <input className={styles.FormInputType} placeholder={placeholder}></input>
+    </div>
+  )
+}
+
 export default function Step1 () {
   return(
-    <div>step111111</div>
+    <div className={styles.step1}>
+      <p className={styles.step1Title}>寄送地址</p>
+      <Formrow>
+        <FormInput width='160px' title='稱謂' placeholder='先生'/>
+        <FormInput width='350px' title='姓名' placeholder='請輸入姓名'/>
+      </Formrow>
+      <Formrow>
+        <FormInput width='255px' title='電話' placeholder='請輸入行動電話'/>
+        <FormInput width='255px' title='Email' placeholder='請輸入電子郵件'/>
+      </Formrow>
+      <Formrow>
+        <FormInput width='160px' title='縣市' placeholder='請選擇縣市'/>
+        <FormInput width='350px' title='地址' placeholder='請輸入地址'/>
+      </Formrow>
+
+    </div>
   )
 }
