001336f46dfeb2dcba4a285477d02d1fc65f3c45
Lan956616 <cd25924218@gmail.com>
2024-08-01 14:47:58 +0800

    complete content of step3

--- src/components/main/pay/pay.js
+++ src/components/main/pay/pay.js
@@ -2,13 +2,14 @@ import styles from './pay.module.css'
 import StepProgress from './stepprogress/stepprogress'
 import Step1 from './step1/step1'
 import Step2 from './step2/step2'
+import Step3 from './step3/step3'
 import ProgressControl from './progresscontrol/progresscontrol'
 
 export default function Pay() {
   return(
     <div className={styles.mainPay}>
       <StepProgress/>
-      <Step2/>
+      <Step3/>
       <ProgressControl/>
     </div>
   )
---
+++ src/components/main/pay/step3/step3.js
@@ -0,0 +1,39 @@
+import styles from './step3.module.css'
+
+
+function Formrow({children}) {
+  return(
+    <div className={styles.Formrow}>
+      {children}
+    </div>
+
+  )
+}
+
+function FormInput({width, title, placeholder}) {
+  return(
+    <div className={styles.FormInput} style={{width:width}}>
+      <p className={styles.FormInputTitle}>{title}</p>
+      <input className={styles.FormInputType} placeholder={placeholder}></input>
+    </div>
+  )
+}
+
+export default function Step3 () {
+  return(
+    <div className={styles.step3}>
+      <p className={styles.step3Title}>付款資訊</p>
+      <Formrow>
+        <FormInput width='350px' title='持卡人姓名' placeholder='John Doe'/>
+      </Formrow>  
+      <Formrow>
+        <FormInput width='350px' title='卡號' placeholder='1111 2222 3333 4444'/>
+      </Formrow>  
+      <Formrow>
+        <FormInput width='255px' title='有效期限' placeholder='MM/YY'/>
+        <FormInput width='255px' title='CVC/CCV' placeholder='123'/>
+      </Formrow>
+
+    </div>
+  )
+}
