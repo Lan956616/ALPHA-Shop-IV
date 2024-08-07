271c5ffad0aa10ae65773bc32ed014191ccc21eb
Lan956616 <cd25924218@gmail.com>
2024-08-07 11:31:35 +0800

    complete function of  plus and minus button, and can correctly show the total of the products

--- src/components/main/cart/cart.js
+++ src/components/main/cart/cart.js
@@ -1,21 +1,49 @@
 import styles from './cart.module.css';
 import minus from './minus.svg';
 import plus from './plus.svg';
+import {useState} from 'react'
 
 
-function CartProduct ({productNumber, productName, productPrice, IMG}){
+function CartProduct ({productNumber, productName, productPrice, IMG,allData, setAllData, ID}){
+
+  function handlePlusClicked(id) {
+    setAllData(allData.map((data)=>{
+      if(data.id === id) {
+        return {...data, quantity: data.quantity+1}
+      } else {
+        return data
+      }
+    }))
+  }
+
+  function handleMinusClicked(id) {
+    setAllData(allData.map((data)=>{
+      if(data.id === id) {
+        //不要商品數量低於1
+        let newQuantity = data.quantity-1
+        if (newQuantity<1) {
+          newQuantity = 1
+        }
+        return {...data, quantity: newQuantity }
+      } else {
+        return data
+      }
+    }))
+  }
+
+
   return(
     <div className={styles.CartProduct}> 
      <img src={IMG} className={styles.CartProductImg}></img>
      <div className={styles.CartProductRightPart}>
        <div className={styles.rightPartTop}>
         <p className={styles.productName}>{productName}</p>
-        <p className={styles.productPrice}>{`$${productPrice}`}</p>
+        <p className={styles.productPrice}>{`$${productPrice * productNumber}`}</p>
        </div>
        <div className={styles.rightPartBottom}>
-        <img src={minus} className={styles.countIcon}></img>
+        <img src={minus} className={styles.countIcon} onClick={()=>{handleMinusClicked(ID)}}></img>
         <p className={styles.productNumber}>{productNumber}</p>
-        <img src={plus} className={styles.countIcon}></img>
+        <img src={plus} className={styles.countIcon} onClick={()=>{handlePlusClicked(ID)}}></img>
 
        </div>
      </div>
@@ -39,15 +67,17 @@ function TotalRow ({title, content}) {
 
 
 export default function Cart({data}) {
+
+  const [allData, setAllData] = useState(data)
   //運用拿到的商品資料 data 先計算總total
-  let totalPrice= 0;
-  for (let i = 0; i < data.length; i++) {
-    totalPrice += parseInt(data[i].price)
+  let totalPrice = 0;
+  for (let i = 0; i < allData.length; i++) {
+    totalPrice += parseInt(allData[i].price) * parseInt(allData[i].quantity)
   }
   return(
     <div className={styles.mainCart}>
       <p className={styles.mainCartTitle}>購物籃</p>
-      {data.map((eachData) => <CartProduct key= {eachData.id} productNumber={eachData.quantity} productName={eachData.name} productPrice = {eachData.price} IMG={eachData.img}/>)
+      {allData.map((eachData) => <CartProduct key= {eachData.id} productNumber={eachData.quantity} productName={eachData.name} productPrice = {eachData.price} IMG={eachData.img} allData={allData} setAllData={setAllData} ID={eachData.id}/>)
       }
       <TotalRow title='運費' content= '免費'/>
       <TotalRow title='小計' content= {totalPrice}/>
