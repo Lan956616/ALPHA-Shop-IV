6b9d44a15e554fbf85ee461fad114117fee2787c
Lan956616 <cd25924218@gmail.com>
2024-08-07 12:38:35 +0800

    complete stepprogress.js

--- src/components/main/pay/stepprogress/stepprogress.js
+++ src/components/main/pay/stepprogress/stepprogress.js
@@ -5,9 +5,9 @@ export default function StepProgress ({step}) {
     <div className={styles.StepProgress}>
       <p className={styles.StepProgressTitle}>結帳</p>
       <ul className={styles.progress}>
-        <li className={`${styles.progressItem} ${styles.Item1}`}>寄送地址</li>
-        <li className={`${styles.progressItem} ${styles.Item2}`}>運送方式</li>
-        <li className={`${styles.progressItem} ${styles.Item3}`}>付款資訊</li>
+        <li className={`${styles.progressItem} ${styles.Item1} ${step > 1? styles.done : ''}`}>寄送地址</li>
+        <li className={`${styles.progressItem} ${styles.Item2} ${step ===2? styles.highlight: ''} ${step > 2? styles.done : ''}`}>運送方式</li>
+        <li className={`${styles.progressItem} ${styles.Item3} ${step===3? styles.highlight: ''}`}>付款資訊</li>
       </ul>
     
     </div>
