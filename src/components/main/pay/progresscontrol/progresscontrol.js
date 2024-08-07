1a87ee48b6a1c63bb3c67819b343eeb2dc96b65d
Lan956616 <cd25924218@gmail.com>
2024-08-07 11:55:25 +0800

    fix bug of state (step) will bigger than 3 or smaller than 1

--- src/components/main/pay/progresscontrol/progresscontrol.js
+++ src/components/main/pay/progresscontrol/progresscontrol.js
@@ -3,11 +3,15 @@ import leftArrow from './leftarrow.svg'
 import styles from './progresscontrol.module.css'
 
 function Button({content, arrowsrc, setStep, step}) {
-
+  function handleNextClick() {
+    if (step === 3) {
+      return
+    } else {
+      setStep(step+1)
+    }
+  }
   return(
-    <div className={styles.button} onClick={()=>{setStep(step+1)
-
-    }}>
+    <div className={styles.button} onClick={handleNextClick}>
       <p className={styles.buttonContent}>{content}</p>
       <img src={arrowsrc} className={styles.buttonImg}/>
     </div>
@@ -15,17 +19,24 @@ function Button({content, arrowsrc, setStep, step}) {
 }
 
 export default function ProgressControl({step, setStep}) {
+
+  function handlePrevious() {
+    if(step === 1) {
+      setStep(1)
+    } else {
+      setStep(step-1)
+    }
+  }
+
   return(
     <div className={styles.progresscontrol}>
       <div className={styles.hr}></div>
       <div className={styles.buttonArea}>
-        {step === 1? '': <div className={styles.stepBack} onClick={()=>{
-          setStep(step-1)
-        }}>
+        {step === 1? '': <div className={styles.stepBack} onClick={handlePrevious}>
           <img src={leftArrow} className={styles.stepBackArrow}/>
           <p className={styles.stepBackContent}>上一步</p>
         </div>}
-        <Button content={step!== 3? '下一步' : '確認下單'} arrowsrc={rightArrow} step={step}  setStep={setStep}/>
+        <Button content={step < 3? '下一步' : '確認下單'} arrowsrc={rightArrow} step={step}  setStep={setStep}/>
       </div>
 
     </div>
